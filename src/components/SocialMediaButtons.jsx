6d97ce503f9503938635dedcf585a2a2607d2270
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-06 15:11:31 -0800

    Adjsutments Icon

--- src/components/SocialMediaButtons.jsx
+++ src/components/SocialMediaButtons.jsx
@@ -19,25 +19,25 @@ export default function SocialMediaButtons() {
       alignItems="center"
       flexDirection="row"
     >
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <InstagramLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <TiktokLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <FacebookLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <RedditLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <TwitchLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <YoutubeLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight">
+      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
         <DiscordLogo size={28} />
       </IconButton>
     </Box>
