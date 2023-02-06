122d5ce91ebf8071721615af0e5c5fa527831dbf
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-06 08:18:06 -0800

    Initial Settings

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primaryBg: '#20262E',
+      darkPurple: '#2D033B',
+      semiDarkPurple: '#810CA8',
+      mainPurple: '#C147E9',
+      lightPurple: '#E5B8F4',
+      beigeLight: '#FFFBF5',
+      beigeDark: '#F7EFE5',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
