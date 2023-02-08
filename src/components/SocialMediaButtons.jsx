88a7966b52c7e42c7c85df2029e5aceec5f1e420
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-08 13:49:56 -0800

    Adding lInks to icons

--- src/components/SocialMediaButtons.jsx
+++ src/components/SocialMediaButtons.jsx
@@ -19,25 +19,46 @@ export default function SocialMediaButtons() {
       alignItems="center"
       flexDirection="row"
     >
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.instagram.com/bamgamesofc/"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <InstagramLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.tiktok.com/@bamgames1"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <TiktokLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.facebook.com/bamgamesofc"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <FacebookLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.reddit.com/user/bamgamesce"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <RedditLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.twitch.tv/bamgames"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <TwitchLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://www.youtube.com/bamgamesoficial"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <YoutubeLogo size={28} />
       </IconButton>
-      <IconButton href="#" className="text-beigeLight hover:text-lightPurple">
+      <IconButton
+        href="https://discord.com/invite/SGJEuWk"
+        className="text-beigeLight hover:text-lightPurple"
+      >
         <DiscordLogo size={28} />
       </IconButton>
     </Box>
