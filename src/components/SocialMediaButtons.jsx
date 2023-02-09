632717e3b733c493f6b30377b8c8ca18ba99178e
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-09 07:37:36 -0800

    Adjusting Icon Sizes

--- src/components/SocialMediaButtons.jsx
+++ src/components/SocialMediaButtons.jsx
@@ -31,43 +31,43 @@ export default function SocialMediaButtons() {
           href="https://www.instagram.com/bamgamesofc/"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <InstagramLogo size={28} />
+          <InstagramLogo size={32} />
         </IconButton>
         <IconButton
           href="https://www.tiktok.com/@bamgames1"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <TiktokLogo size={28} />
+          <TiktokLogo size={32} />
         </IconButton>
         <IconButton
           href="https://www.facebook.com/bamgamesofc"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <FacebookLogo size={28} />
+          <FacebookLogo size={32} />
         </IconButton>
         <IconButton
           href="https://www.reddit.com/user/bamgamesce"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <RedditLogo size={28} />
+          <RedditLogo size={32} />
         </IconButton>
         <IconButton
           href="https://www.twitch.tv/bamgames"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <TwitchLogo size={28} />
+          <TwitchLogo size={32} />
         </IconButton>
         <IconButton
           href="https://www.youtube.com/bamgamesoficial"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <YoutubeLogo size={28} />
+          <YoutubeLogo size={32} />
         </IconButton>
         <IconButton
           href="https://discord.com/invite/SGJEuWk"
           className="text-beigeLight hover:text-lightPurple"
         >
-          <DiscordLogo size={28} />
+          <DiscordLogo size={32} />
         </IconButton>
       </Box>
     </IconContext.Provider>
