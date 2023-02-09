b1d1f8fd2eb818201b84b957ccb0b008e2c3a024
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-09 07:36:27 -0800

    Social Media Icons Color

--- src/components/SocialMediaButtons.jsx
+++ src/components/SocialMediaButtons.jsx
@@ -1,6 +1,7 @@
 import { Box, IconButton } from '@mui/material';
 
 import {
+  IconContext,
   TiktokLogo,
   InstagramLogo,
   FacebookLogo,
@@ -12,55 +13,63 @@ import {
 
 export default function SocialMediaButtons() {
   return (
-    <Box
-      mt="10px"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      flexDirection="row"
+    <IconContext.Provider
+      value={{
+        color: '#F7EFE5',
+        weight: 'bold',
+        mirrored: false,
+      }}
     >
-      <IconButton
-        href="https://www.instagram.com/bamgamesofc/"
-        className="text-beigeLight hover:text-lightPurple"
+      <Box
+        mt="10px"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="row"
       >
-        <InstagramLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://www.tiktok.com/@bamgames1"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <TiktokLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://www.facebook.com/bamgamesofc"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <FacebookLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://www.reddit.com/user/bamgamesce"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <RedditLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://www.twitch.tv/bamgames"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <TwitchLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://www.youtube.com/bamgamesoficial"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <YoutubeLogo size={28} />
-      </IconButton>
-      <IconButton
-        href="https://discord.com/invite/SGJEuWk"
-        className="text-beigeLight hover:text-lightPurple"
-      >
-        <DiscordLogo size={28} />
-      </IconButton>
-    </Box>
+        <IconButton
+          href="https://www.instagram.com/bamgamesofc/"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <InstagramLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://www.tiktok.com/@bamgames1"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <TiktokLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://www.facebook.com/bamgamesofc"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <FacebookLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://www.reddit.com/user/bamgamesce"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <RedditLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://www.twitch.tv/bamgames"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <TwitchLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://www.youtube.com/bamgamesoficial"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <YoutubeLogo size={28} />
+        </IconButton>
+        <IconButton
+          href="https://discord.com/invite/SGJEuWk"
+          className="text-beigeLight hover:text-lightPurple"
+        >
+          <DiscordLogo size={28} />
+        </IconButton>
+      </Box>
+    </IconContext.Provider>
   );
 }
