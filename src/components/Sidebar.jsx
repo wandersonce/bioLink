3bfc5aa2b066879cf476294acc1011a34ec3f7dd
Wanderson Castro <wandersonce1@hotmail.com>
2023-02-16 08:03:00 -0800

    adding sidebar items

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { signOut } from 'next-auth/react';
 import Link from 'next/link';
 import {
   Sidebar as ProSidebar,
@@ -9,7 +10,14 @@ import {
 // import 'react-pro-sidebar/dist/css/styles.css';
 import { Box, IconButton, Typography } from '@mui/material';
 
-import { MenuOutlined } from '@mui/icons-material';
+import {
+  MenuOutlined,
+  HomeOutlined,
+  Redeem,
+  Handshake,
+  Devices,
+  Logout,
+} from '@mui/icons-material';
 
 const Item = ({ title, to, icon, selected, setSelected }) => {
   return (
@@ -27,8 +35,6 @@ export default function Sidebar({ session }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [selected, setSelected] = useState('Dashboard');
   const { collapseSidebar } = useProSidebar();
-
-  console.log(session);
 
   function changeCollapseState() {
     setIsCollapsed(!isCollapsed);
@@ -62,7 +68,7 @@ export default function Sidebar({ session }) {
         },
       }}
     >
-      <ProSidebar collapseSidebar={isCollapsed}>
+      <ProSidebar collapseSidebar={isCollapsed} display="flex">
         <Menu iconShape="square">
           <MenuItem
             onClick={() => changeCollapseState()}
@@ -97,8 +103,8 @@ export default function Sidebar({ session }) {
                   alt="Profile User"
                   width="100px"
                   height="100px"
-                  src={session.user.imgUrl}
-                  style={{ cursor: 'pointer', borderRadius: '50%' }}
+                  src={session?.user.imgUrl}
+                  style={{ borderRadius: '50%' }}
                 />
               </Box>
               <Box textAlign="center">
@@ -109,7 +115,7 @@ export default function Sidebar({ session }) {
                   fontSize="1.5rem"
                   sx={{ m: '10px 0 0 0' }}
                 >
-                  {session.user.name}
+                  {session?.user.name}
                 </Typography>
                 <Typography
                   fontSize="1.2rem"
@@ -117,12 +123,66 @@ export default function Sidebar({ session }) {
                   color="#E5B8F4"
                   textTransform="uppercase"
                 >
-                  {session.user.role}
+                  {session?.user.role}
                 </Typography>
               </Box>
             </Box>
           )}
+
+          {/* MENU ITEMS */}
+          <Box paddingLeft={isCollapsed ? undefined : '10%'}>
+            <Item
+              title="Dashboard"
+              to="/dashboard"
+              icon={<HomeOutlined />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Typography
+              variant="h6"
+              color="#6B728E"
+              sx={{ m: '15px 0 5px 20px' }}
+            >
+              Data
+            </Typography>
+            <Item
+              title="Wishlist Items"
+              to="/wishList"
+              icon={<Redeem />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+            <Item
+              title="Partners List"
+              to="/partners"
+              icon={<Handshake />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+
+            <Item
+              title="Setup Parts"
+              to="/pcparts"
+              icon={<Devices />}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          </Box>
         </Menu>
+        <button
+          onClick={() => signOut()}
+          className="flex gap-2 absolute bottom-4 left-8"
+        >
+          {!isCollapsed ? (
+            <>
+              <Logout />
+              Sign Out
+            </>
+          ) : (
+            <Logout />
+          )}
+        </button>
       </ProSidebar>
     </Box>
   );
