a74bcfdcdd83b92d761fd7064743065fe1ca0b51
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-08 12:14:14 -0800

    Adding discord banner

--- src/components/CommercialSlider.jsx
+++ src/components/CommercialSlider.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
@@ -7,29 +7,35 @@ export default function CommercialSlider() {
     <Box mt="40px">
       <Carousel infiniteLoop swipeable showThumbs={false}>
         <div>
-          <div
-            style={{
-              backgroundImage: 'url("./background1.webp")',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }}
-            className="bg-baseGray rounded-lg min-h-[200px] h-full flex flex-col justify-center items-center text-beigeDark"
+          <Button
+            href="https://discord.com/invite/SGJEuWk"
+            style={{ width: '100%' }}
           >
-            <Typography
-              variant="h5"
-              fontSize="32px"
-              fontWeight="bold"
-              textAlign="center"
-              sx={{ padding: '0 24px 0 24px', color: '#FFF' }}
+            <div
+              style={{
+                backgroundImage: 'url("./discordBanner.png")',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                width: '100%',
+              }}
+              className="bg-baseGray rounded-lg min-h-[300px] h-full flex flex-col justify-center items-center text-beigeDark"
             >
-              Promote your Brand
-              <br /> Here!
-            </Typography>
-            <Typography sx={{ color: '#FFF' }} variant="span">
-              Send me an email, and know how.
-            </Typography>
-          </div>
+              {/* <Typography
+                variant="h5"
+                fontSize="32px"
+                fontWeight="bold"
+                textAlign="center"
+                sx={{ padding: '0 24px 0 24px', color: '#FFF' }}
+              >
+                Promote your Brand
+                <br /> Here!
+              </Typography>
+              <Typography sx={{ color: '#FFF' }} variant="span">
+                Send me an email, and know how.
+              </Typography> */}
+            </div>
+          </Button>
         </div>
         <div>
           <div
@@ -39,32 +45,7 @@ export default function CommercialSlider() {
               backgroundRepeat: 'no-repeat',
               backgroundSize: 'cover',
             }}
-            className="bg-baseGrayDark rounded-lg min-h-[200px] h-full flex flex-col justify-center items-center text-beigeDark"
-          >
-            <Typography
-              variant="h5"
-              fontSize="32px"
-              fontWeight="bold"
-              textAlign="center"
-              sx={{ padding: '0 24px 0 24px', color: '#FFF' }}
-            >
-              Rent this space,
-              <br /> Show your Product!
-            </Typography>
-            <Typography sx={{ color: '#FFF' }} variant="span">
-              Send me an email, and know how.
-            </Typography>
-          </div>
-        </div>
-        <div>
-          <div
-            style={{
-              backgroundImage: 'url("./background3.jpg")',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              backgroundSize: 'cover',
-            }}
-            className="bg-grayLight rounded-lg min-h-[200px] h-full flex flex-col justify-center items-center text-beigeDark"
+            className="bg-baseGrayDark rounded-lg min-h-[300px] h-full flex flex-col justify-center items-center text-beigeDark"
           >
             <Typography
               variant="h5"
