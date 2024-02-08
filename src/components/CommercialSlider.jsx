936c3e9f2b0f8ef20e0cc36e38cdd71de1ff315a
Wanderson Castro <wandersonce1@hotmail.com>
2024-02-08 12:34:01 -0800

    Adding mobileBanner

--- src/components/CommercialSlider.jsx
+++ src/components/CommercialSlider.jsx
@@ -1,8 +1,12 @@
 import { Box, Button, Typography } from '@mui/material';
 import { Carousel } from 'react-responsive-carousel';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 
 export default function CommercialSlider() {
+  const matches = useMediaQuery('(max-width:640px)');
+  console.log(matches);
+
   return (
     <Box mt="40px">
       <Carousel infiniteLoop swipeable showThumbs={false}>
@@ -12,13 +16,23 @@ export default function CommercialSlider() {
             style={{ width: '100%' }}
           >
             <div
-              style={{
-                backgroundImage: 'url("./discordBanner.png")',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'cover',
-                width: '100%',
-              }}
+              style={
+                matches
+                  ? {
+                      backgroundImage: 'url("./discordBannerMob.png")',
+                      backgroundPosition: 'center',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      width: '100%',
+                    }
+                  : {
+                      backgroundImage: 'url("./discordBanner.png")',
+                      backgroundPosition: 'center',
+                      backgroundRepeat: 'no-repeat',
+                      backgroundSize: 'cover',
+                      width: '100%',
+                    }
+              }
               className="bg-baseGray rounded-lg min-h-[300px] h-full flex flex-col justify-center items-center text-beigeDark"
             >
               {/* <Typography
